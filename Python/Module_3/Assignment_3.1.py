922d4efd5fd7705a61f7561e92fe30ed1345fe9b
Gary Jia <jiakeke@gmail.com>
2023-09-02 07:41:59 +0300

    Answer for question 3.1.

--- Python/Module_3/Assignment_3.1.py
+++ Python/Module_3/Assignment_3.1.py
@@ -1,3 +1,13 @@
+"""
+3. Conditional structures (if)
+
+    3.1. Write a program that asks a fisher the length of a zander in
+         centimeters. If the zander does not fulfill the size limit, the
+         program instructs to release the fish back into the lake and notifies
+         the user of how many centimeters below the size limit the caught fish
+         was. A zander must be 42 centimeters or longer to meet the size limit.
+"""
+
 size_limit = 42
 
 length = int(input('How many centimeters is the zander you caught?\n'))
