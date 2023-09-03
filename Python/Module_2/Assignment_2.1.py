298cb35868749a7d1f58de333be6dfdc6e5d8de4
Gary Jia <jiakeke@gmail.com>
2023-09-03 21:06:11 +0300

    Answer for question 2.1

--- Python/Module_2/Assignment_2.1.py
+++ Python/Module_2/Assignment_2.1.py
@@ -1,3 +1,15 @@
+"""
+2. Variables and interactive programs
+
+    1.1. Write a program that asks your name and then greets you by your name,
+         Examples:
+
+             - If you enter Viivi as your name, the program will greet you with
+               Hello, Viivi!.
+             - If you enter Ahmed as your name, the program will greet you with
+               Hello, Ahmed!.
+
+"""
 
 name = input('Please input your name: ')
 print(f'Hello, {name}!')
