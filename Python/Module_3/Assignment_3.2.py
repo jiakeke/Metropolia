09ebb02972ba2384b4e553c799bd054652dbfb20
Gary Jia <jiakeke@gmail.com>
2023-09-03 11:38:27 +0300

    Answer for question 3.2.

--- Python/Module_3/Assignment_3.2.py
+++ Python/Module_3/Assignment_3.2.py
@@ -1,7 +1,17 @@
-#LUX: upper-deck cabin with a balcony.
-#A: above the car deck, equipped with a window.
-#B: windowless cabin above the car deck.
-#C: windowless cabin below the car deck.
+"""
+3. Conditional structures (if)
+
+    3.2. Write a program that asks the user to enter the cabin class of a
+         cruise ship and then prints out a written description according to the
+         list below. You must use an if/elif/else structure in your solution.
+
+             - LUX: upper-deck cabin with a balcony.
+             - A: above the car deck, equipped with a window.
+             - B: windowless cabin above the car deck.
+             - C: windowless cabin below the car deck.
+
+         If the user enters an invalid cabin class, the program outputs an error message Invalid cabin class.
+"""
 
 cabin_class = input("Could you please input the cabin class of the cruise ship?\n").upper()
 
