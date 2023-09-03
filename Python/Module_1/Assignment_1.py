033a44504420681f0ef541cbd31e63dbef8be6d0
Gary Jia <jiakeke@gmail.com>
2023-09-03 21:01:20 +0300

    Answer for question 3.1.

--- Python/Module_1/Assignment_1.py
+++ Python/Module_1/Assignment_1.py
@@ -1,1 +1,13 @@
-print('Hello, Jia Ke')
+"""
+1. First program and deployment of version control
+    1.1. Install the PyCharm IDE. Write a program that greets you by your own
+         name. If your name was Viivi Virta, the output of the program would be
+         Hello, Viivi Virta!.
+
+    1.2. Create yourself a GitHub account and create a new repository for your
+         Python exercises. Connect the repository to PyCharm so that your
+         exercise projects are stored on the repository. Make sure that you are
+         able to pull, commit and push your changes to the repository.
+"""
+
+print("Hello, Jia Ke")
