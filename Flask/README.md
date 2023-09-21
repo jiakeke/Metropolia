d2cd6d133e4e40c61e5f3a44561acfadcab2979f
Gary Jia <jiakeke@gmail.com>
2023-09-21 23:13:52 +0300

    Add the demo code to the repository.

---
+++ Flask/README.md
@@ -0,0 +1,14 @@
+# Flask demo for Ke Jia (Gary)
+
+## Installation
+The code was tested using python version 3.9.13, and the required dependencies for running the code can be found in the file `requirements.txt`. To install the dependencies in a virtual environment, use the command:
+```
+pip install -r requirements.txt
+```
+
+## Run
+To run the demo, inside of your terminal navigate to the top level directory containing this `README.md` and run the following command:
+```
+flask run
+```
+This should open a web server at: `http://127.0.0.1:5000`. You can view this url in your browser.
---
+++ Flask/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from transformers import pipeline
+
+app = Flask(__name__)
+
+@app.route("/", methods=['POST', 'GET'])
+def index():
+    text = ''
+    text_fi = ''
+    if request.method == 'POST':
+        text = request.form['text']
+        if text:
+            pipe = pipeline("translation", model="Helsinki-NLP/opus-mt-en-fi")
+            result = pipe(text)
+            if result:
+                print(text)
+                print(result)
+                text_fi = result[0].get('translation_text', '')
+
+
+    return render_template('index.html', text=text, text_fi=text_fi)
