63044532163691e909507b5723e0fb5572445b1b
Gary Jia <jiakeke@gmail.com>
2023-11-17 13:13:46 +0200

    Fix the assignment 5 a bit switch common year to not a leap year.

--- Javascripts/module_1/assignment_05/assignment_05.js
+++ Javascripts/module_1/assignment_05/assignment_05.js
@@ -1,14 +1,16 @@
 'use strict';
 
 let year, year_type;
-year = parseInt(prompt("Please a year"));
+year = parseInt(prompt("Please input a year"));
 
-if (year % 400 == 0){
-    year_type = 'leap';
+if (year == 0) {
+    year_type = ' not ';
+} else if (year % 400 == 0){
+    year_type = '';
 } else if (year % 4 == 0 & year % 100 != 0) {
-    year_type = 'leap';
+    year_type = '';
 } else {
-    year_type = 'common';
+    year_type = ' not ';
 }
 
-document.querySelector('#result').innerHTML = `${year} is a ${year_type} year.`;
+document.querySelector('#result').innerHTML = `${year} is ${year_type}a leap year.`;
