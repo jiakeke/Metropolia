5d56b5bbe1c51a22ca2b88beaf3ba6a394ffcf42
Gary Jia <jiakeke@gmail.com>
2023-12-03 11:20:23 +0200

    fix a bit

--- Javascripts/module_1/assignment_08/assignment_08.js
+++ Javascripts/module_1/assignment_08/assignment_08.js
@@ -3,7 +3,7 @@
 function is_leap_year(year) {
     if (year % 400 == 0){
         return true;
-    } else if (year % 4 == 0 & year % 100 != 0) {
+    } else if (year % 4 == 0 && year % 100 != 0) {
         return true;
     } else {
         return false;
